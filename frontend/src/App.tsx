3f05c84b7a09f555e42c0b5b9686cf15da79789d
YanFelipy22 <yan-felipy@hotmail.com>
2021-09-07 00:49:44 -0300

    Correção na div, inserindo classe container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,33 +5,33 @@ import BarChart from "components/BarChart";
 import DonutChart from "components/DonutCharts";
 
 function App() {
-  return (
-    <>
-    <NavBar/>
-    <div>
-      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-      
-      <div className="row px-3">
-    <div className="col-sm-6">
-      <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-      <BarChart />
-    </div>
-    <div className="col-sm-6">
-      <h5 className="text-center text-secondary">Todas vendas</h5>
-      <DonutChart />
-    </div>
-      </div>
+return (
+<>
+  <NavBar />
+  <div className="container">
+    <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-      <div className="py-3">
+    <div className="row px-3">
+      <div className="col-sm-6">
+        <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+        <BarChart />
+      </div>
+      <div className="col-sm-6">
+        <h5 className="text-center text-secondary">Todas vendas</h5>
+        <DonutChart />
+      </div>
+    </div>
+
+    <div className="py-3">
       <h2 className="text-primary">Todas vendas</h2>
-      </div>
-
-
-      <DataTable />
     </div>
-    <Footer/>
-    </>
-  );
+
+
+    <DataTable />
+  </div>
+  <Footer />
+</>
+);
 }
 
 export default App;
