5e48cc107f4b5d78a38cd485b80a8e2e31a5610d
YanFelipy22 <yan-felipy@hotmail.com>
2021-09-08 20:54:33 -0300

    Camadas (Layers)

---
+++ backend/src/main/java/com/devsuperioryan/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperioryan.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperioryan.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller,Long>{
+
+}
