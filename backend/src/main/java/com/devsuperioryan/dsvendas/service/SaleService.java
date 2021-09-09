b31bb18b37a18d0ae7507c5ba671cb78afdc053a
YanFelipy22 <yan-felipy@hotmail.com>
2021-09-08 23:58:04 -0300

    Procura por grupo

--- backend/src/main/java/com/devsuperioryan/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperioryan/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.devsuperioryan.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperioryan.dsvendas.dto.SaleDTO;
+import com.devsuperioryan.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperioryan.dsvendas.dto.SaleSumDTO;
 import com.devsuperioryan.dsvendas.entities.Sale;
 import com.devsuperioryan.dsvendas.repositories.SaleRepository;
 import com.devsuperioryan.dsvendas.repositories.SellerRepository;
@@ -26,4 +30,14 @@ public class SaleService {
 		 Page <Sale> result = repository.findAll(pageable);
 	    return result.map(x -> new SaleDTO(x));
 	 }
+	
+	@Transactional(readOnly =true) 
+	public List<SaleSumDTO> amountGroupedBySeller() {
+	      return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly =true) 
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+	      return repository.successGroupedBySeller();
+	}
 	}
